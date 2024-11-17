90f5c4ed17c39f6f17d53a925a08a13820dca43a
BSPANGLER <bspangler21@alumni.uwosh.edu>
2024-11-17 00:38:40 -0600

    Update subtitle in App component to include 'Engineer'

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 	return (
 		<>
 			<h1>Brett Spangler</h1>
-			<h2>Passionate and Driven Software Developer</h2>
+			<h2>Passionate and Driven Software Developer/Engineer</h2>
 			<div>
 				<a
 					href="https://www.linkedin.com/in/brett-spangler/"
