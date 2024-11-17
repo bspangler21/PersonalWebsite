c34b681e163bb85e596c232e77d5a60aa41d6b3d
BSPANGLER <bspangler21@alumni.uwosh.edu>
2024-11-17 00:33:04 -0600

    Add cover images and notable GitHub projects section to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import commencementPhoto from "./assets/Commencement.png";
+import headshot from "./assets/Headshot.png";
+import workProfilePhoto from "./assets/WorkProfile.jpg";
 import "./App.css";
+import utilStyles from "./styles/utilStyles.module.css";
 
 function App() {
 	return (
@@ -14,10 +17,77 @@ function App() {
 				>
 					<img
 						src={commencementPhoto}
-						className="logo"
+						className="coverImage"
 						alt="Brett Spangler speaking at his UWO graduation ceremony"
 					/>
 				</a>
+				<a
+					href="https://www.linkedin.com/in/brett-spangler/"
+					target="_blank"
+					rel="noopener"
+				>
+					<img
+						src={headshot}
+						className="coverImage"
+						alt="Brett Spangler's professional headshot'"
+					/>
+				</a>
+				<a
+					href="https://www.linkedin.com/in/brett-spangler/"
+					target="_blank"
+					rel="noopener"
+				>
+					<img
+						src={workProfilePhoto}
+						className="coverImage profile"
+						alt="Brett Spangler work profile photo"
+					/>
+				</a>
+			</div>
+			<div
+				className={utilStyles.leftAlign + " " + utilStyles.marginSpace}
+			>
+				<h3>Notable GitHub Projects</h3>
+
+				<ul>
+					<li>
+						<a
+							rel="noopener"
+							href="https://github.com/bspangler21/FrontendTemplate"
+							target="_blank"
+						>
+							Frontend Template
+						</a>
+					</li>
+					<li>
+						<a
+							rel="noopener"
+							href="https://github.com/bspangler21/WorryApp"
+							target="_blank"
+						>
+							W.O.R.R.Y. App
+						</a>
+					</li>
+					<li>
+						<a
+							rel="noopener"
+							href="https://github.com/bspangler21/Golf-Tracker"
+							target="_blank"
+						>
+							Golf Tracker
+						</a>
+					</li>
+					<li>
+						<a
+							rel="noopener"
+							href="https://github.com/bspangler21/Planner"
+							target="_blank"
+						>
+							SharePoint Framework (SPFx) Web Part with Planner
+							integration
+						</a>
+					</li>
+				</ul>
 			</div>
 		</>
 	);
